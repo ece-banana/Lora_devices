5bb9d6421f938a3665d79e1f074f82e0fadad470
Andrei Shilikov <andrei.shilikov@oswego.edu>
2024-07-03 12:20:55 -0400

    Update example.py
    micro comment

--- example.py
+++ example.py
@@ -5,14 +5,14 @@ port = serial.Serial(PORT, timeout=1) # timeout important to read empty buffer, 
 
 if(not port.is_open):
     port.open()
-
+# check buffer for information
 def recv(raw=0):
     if(port.in_waiting):
         if(raw):
             print(port.read(100))
         else:
             print(port.read(100).decode())
-
+# simple function to send data
 def send(s, raw=0):
     header = b'\x00\x01\x17' # header first 1st & 2nd bytes address 3rd "chan." (formula 420 + chan#) in our case 0x17 = 23 = 420 + 23 = 433 MHz 
     if(raw):
